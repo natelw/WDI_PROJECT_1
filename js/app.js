d8c57837ac651983ed4b1ca51e7e63ad3e0dbce5
natelw <nwelfare@gmail.com>
2017-10-06 15:24:22 +0100

    keypress handler

--- js/app.js
+++ js/app.js
@@ -1,34 +1,66 @@
-$(setup);
+console.log('up and running');
+
+// keypress handler
+let pressedLeft = 'false';
+let pressedRight = 'false';
+let pressedUp = 'false';
+let pressedDown = 'false';
+
+document.addEventListener('keydown',(e) =>{
+  console.log(e.keyCode);
+  switch(e.keyCode){
+    case 65: pressedLeft = true;
+      break;
+    case 87: pressedUp = true;
+      break;
+    case 68: pressedRight = true;
+      break;
+    case 83: pressedDown = true;
+      break;
+  }
+}, false);
+
+document.addEventListener('keyup',(e) => {
+  switch(e.keyCode){
+    case 65: pressedLeft = false;
+      break;
+    case 87: pressedUp = false;
+      break;
+    case 68: pressedRight = false;
+      break;
+    case 83: pressedDown = false;
+      break;
+  }
+
+});
 
 
-function setup(){
-
-
-  console.log('up and running');
-}
 
 
 
 
-const map = [
-  [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
-  [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b]
-];
+
+
+//
+// const map = [
+//   [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+//   [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b]
+// ];
