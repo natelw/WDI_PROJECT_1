16a78d79c82838966ec2d8c15c650321a36ad368
natelw <nwelfare@gmail.com>
2017-10-06 15:50:46 +0100

    keypress handler

--- js/app.js
+++ js/app.js
@@ -1,34 +1,43 @@
 console.log('up and running');
-
+let x = 0;
 // keypress handler
 let pressedLeft = 'false';
 let pressedRight = 'false';
 let pressedUp = 'false';
 let pressedDown = 'false';
-
+let pressedSpace = 'false';
 document.addEventListener('keydown',(e) =>{
   console.log(e.keyCode);
   switch(e.keyCode){
-    case 65: pressedLeft = true;
+    case 37: pressedLeft = true;
       break;
-    case 87: pressedUp = true;
+    case 38: pressedUp = true;
       break;
-    case 68: pressedRight = true;
+    case 39: pressedRight = true;
       break;
-    case 83: pressedDown = true;
+    case 40: pressedDown = true;
+      break;
+    case 32: pressedSpace = true;
       break;
   }
+  pressedDown === true ? console.log('working'):x=0 ;
+  pressedUp === true ? console.log('working'): x= 0;
+  pressedLeft === true ? console.log('working'):x = 0 ;
+  pressedRight === true ? console.log('working'): x = 0;
+  pressedSpace === true ? console.log('working'): x= 0;
 }, false);
 
 document.addEventListener('keyup',(e) => {
   switch(e.keyCode){
-    case 65: pressedLeft = false;
+    case 37: pressedLeft = false;
       break;
-    case 87: pressedUp = false;
+    case 38: pressedUp = false;
       break;
-    case 68: pressedRight = false;
+    case 39: pressedRight = false;
       break;
-    case 83: pressedDown = false;
+    case 40: pressedDown = false;
+      break;
+    case 32: pressedSpace = false;
       break;
   }
 
