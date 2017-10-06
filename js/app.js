c818c2191eb4484b2bcb5913c75191ca082c75bb
natelw <nwelfare@gmail.com>
2017-10-06 14:55:27 +0100

    planning stages

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log('up and running');
+$(setup);
+
+
+function setup(){
+
+
+  console.log('up and running');
+}
+
+
+
+
+const map = [
+  [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,g,b],
+  [b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b]
+];
